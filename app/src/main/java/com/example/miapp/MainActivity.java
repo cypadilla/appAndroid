965ca86580229b45deee75e65499132310acb18c
Cristian Padilla <Cristancho2011@gmail.com>
2020-05-07 12:31:30 -0500

    Se agrego la activity para autenticacion por correo electronico

--- app/src/main/java/com/example/miapp/MainActivity.java
+++ app/src/main/java/com/example/miapp/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText numero1;
     private EditText numero2;
-    private TextView resultado;
+    private Button botonRegistro,botonIngresar;
 
 
     @Override
@@ -19,22 +21,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numero1 = (EditText) findViewById(R.id.numero1);
-        numero2 = (EditText) findViewById(R.id.numero2);
-        resultado = (TextView) findViewById(R.id.resultado);
+        botonRegistro = (Button) findViewById(R.id.registroBoton);
+        botonRegistro.setOnClickListener(this);
+
+        botonIngresar = (Button) findViewById(R.id.ingresarBoton);
+        botonIngresar.setOnClickListener(this);
+
+        numero1 = (EditText) findViewById(R.id.correoElectronico);
+        numero2 = (EditText) findViewById(R.id.password);
+
     }
 
-    public void Sumar(View view){
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ingresarBoton:
+                validacionIngreso();
+                break;
+            case R.id.registroBoton:
+                Intent intencion = new Intent(this,Registro.class);
+                startActivity(intencion);
+                break;
+
+        }
+
+    }
+
+    public void validacionIngreso(){
         String valorNumero1 = numero1.getText().toString();
         String valorNumero2 = numero2.getText().toString();
 
-        int valorN1 = Integer.parseInt(valorNumero1);
-        int valorN2 = Integer.parseInt(valorNumero2);
 
-        int suma = valorN1+valorN2;
 
-        String resultadoFuncion = String.valueOf(suma);
-        resultado.setText(resultadoFuncion);
+       ;
 
     }
+
 }
---
+++ app/src/main/java/com/example/miapp/Registro.java
@@ -0,0 +1,87 @@
+package com.example.miapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+  private EditText correoUsuario;
+  private EditText contraUsuario;
+  private Button registrarBoton;
+  private Button volverButton;
+  private FirebaseAuth mAuth;
+  private ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        correoUsuario = (EditText) findViewById(R.id.nombreRegistro);
+        contraUsuario = (EditText) findViewById(R.id.contraRegistro);
+        registrarBoton = (Button) findViewById(R.id.registroBoton1);
+        registrarBoton.setOnClickListener(this);
+        volverButton = (Button) findViewById(R.id.regresarInicio);
+        volverButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registroBoton1:
+                registroUsuario();
+                break;
+            case R.id.regresarInicio:
+                Intent intencion = new Intent(this,MainActivity.class);
+                startActivity(intencion);
+        }
+
+    }
+
+    public void registroUsuario(){
+
+        String correo = correoUsuario.getText().toString().trim();
+        String contra = contraUsuario.getText().toString().trim();
+
+        if(TextUtils.isEmpty(correo)){
+            Toast.makeText(this,"Ingrese algo no sea concha",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(contra)){
+            Toast.makeText(this,"Ingrese algo no sea concha",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registro en proceso...");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(correo,contra)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Registro.this,"Se registro correctamente",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(Registro.this,"No Se registro correctamente",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+}
